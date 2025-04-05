056f9f20451b50508e36ec209b6acd1c1464bcfd
artemObrazumov <a.obraz60022006@gmail.com>
2025-04-05 17:30:03 +0300

    removed shadow from make order button

--- app/src/main/java/com/artemObrazumov/drinkin/cart/presentation/cart/components/MakeOrderCard.kt
+++ app/src/main/java/com/artemObrazumov/drinkin/cart/presentation/cart/components/MakeOrderCard.kt
@@ -1,20 +1,16 @@
 package com.artemObrazumov.drinkin.cart.presentation.cart.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -30,13 +26,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.artemObrazumov.drinkin.app.ui.theme.darkTextColor
@@ -100,13 +93,7 @@ fun MakeOrderCard(
             Button(
                 onClick = { onMakeOrderClicked() },
                 modifier = Modifier
-                    .fillMaxHeight()
-                    .shadow(
-                        elevation = 20.dp,
-                        spotColor = MaterialTheme.colorScheme.primary,
-                        ambientColor = MaterialTheme.colorScheme.primary,
-                        shape = CircleShape
-                    ),
+                    .fillMaxHeight(),
                 enabled = buttonEnabled
             ) {
                 Box(
